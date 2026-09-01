57e08c857ca9cff95286c063970d7750608c987b
Akash Verma <aakash.vermaa06@gmail.com>
2026-09-01 21:36:28 +0530

    chore: remove redundant scratch scripts, boilerplate assets, and obsolete plan files

--- scratch/gen_favicon.js
+++
@@ -1,4 +0,0 @@
-const fs = require('fs');
-const base64Ico = 'AAABAAEAAQEAAAEAIAAwAAAAFgAAACgAAAABAAAAAgAAAAEAIAAAAAAACAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAgAAAAA==';
-fs.writeFileSync('src/app/favicon.ico', Buffer.from(base64Ico, 'base64'));
-console.log('favicon.ico created');
